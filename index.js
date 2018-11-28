2f33b76c7e7f8e18f4bf2556d15545ca36fe4e4b
mientki <37380727+mientki@users.noreply.github.com>
2018-11-28 10:02:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var mqtt    = require('mqtt');
 module.exports = function(homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	homebridge.registerAccessory("homebridge-mqtt-sonoffrf-receiver", "mqtt-sonoffrf-receiver", "homebridge-mqttthing", "mqttthing", RfSensorAccessory);
+	homebridge.registerAccessory("homebridge-mqtt-433-sensor", "mqtt-433-sensor", "homebridge-mqttthing", "mqttthing", RfSensorAccessory);
 }
 
 function RfSensorAccessory(log, config) {
